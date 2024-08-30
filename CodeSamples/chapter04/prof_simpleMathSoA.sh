26866349d9d3583ef7c36d2d0f7103c1b5e05229
hexu <hexu_1985@sina.com>
2024-08-30 18:28:21 +0800

    update CodeSamples/chapter04

---
+++ CodeSamples/chapter04/prof_simpleMathSoA.sh
@@ -0,0 +1,4 @@
+
+# nvprof --devices 0 --metrics gld_efficiency,gst_efficiency ./simpleMathAoS
+sudo /usr/local/NVIDIA-Nsight-Compute/ncu --metrics smsp__sass_average_data_bytes_per_sector_mem_global_op_ld.pct,smsp__sass_average_data_bytes_per_sector_mem_global_op_st.pct ./simpleMathSoA
+
